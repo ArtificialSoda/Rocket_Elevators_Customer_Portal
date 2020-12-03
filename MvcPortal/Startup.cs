2b793c8b98aa0a108d993b8916ef06eb1e4b4349
Fabien H. Dimitrov <dimitrov.fabien@gmail.com>
2020-12-03 07:15:12 -0500

    Working auth page - needs minor fixes

--- MvcPortal/Startup.cs
+++ MvcPortal/Startup.cs
@@ -24,6 +24,7 @@ namespace MvcPortal
         public void ConfigureServices(IServiceCollection services)
         {
             services.AddControllersWithViews();
+            services.AddRazorPages();
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
@@ -36,7 +37,6 @@ namespace MvcPortal
             else
             {
                 app.UseExceptionHandler("/Home/Error");
-                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
                 app.UseHsts();
             }
             app.UseHttpsRedirection();
@@ -44,6 +44,7 @@ namespace MvcPortal
 
             app.UseRouting();
 
+            app.UseAuthentication();
             app.UseAuthorization();
 
             app.UseEndpoints(endpoints =>
@@ -51,6 +52,7 @@ namespace MvcPortal
                 endpoints.MapControllerRoute(
                     name: "default",
                     pattern: "{controller=Home}/{action=Index}/{id?}");
+                endpoints.MapRazorPages();
             });
         }
     }
